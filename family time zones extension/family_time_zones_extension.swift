f31f4749d7a8363b8fa11f0d05147263f9299195
tjnielsen <tjnielse@adobe.com>
2025-04-15 12:20:12 -0400

    Removing ref.

---
+++ family time zones extension/family_time_zones_extension.swift
@@ -0,0 +1,16 @@
+//
+//  family_time_zones_extension.swift
+//  family time zones extension
+//
+//  Created by TJ Nielsen on 4/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+// This file is a placeholder and should be removed from the project.
+// It exists only to satisfy the build system reference.
+// Please remove it from the target in Xcode by:
+// 1. Select this file in the Project Navigator
+// 2. Right-click and select "Delete"
+// 3. Choose "Remove Reference" when prompted
